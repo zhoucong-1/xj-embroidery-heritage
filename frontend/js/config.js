040075c80d7004765a1d44f7dba8b28b6d8578a4
Administrator <admin@test.com>
2026-04-20 13:02:19 +0800

    更新API配置

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,7 +1,7 @@
 // API配置
 const API_CONFIG = {
   // Cloudflare Worker API地址
-  BASE_URL: 'https://xj-embroidery-api.yourusername.workers.dev',
+  BASE_URL: 'https://xj-embroidery-api.18662995253.workers.dev',
   // 本地开发地址
   // BASE_URL: 'http://localhost:5184'
 };
